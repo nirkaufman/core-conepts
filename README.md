fcb20615a4847b85d7ebc6d7c273c13c1f247d66
nirkaufman <nirkaufman@gmail.com>
2020-01-12 22:01:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Redux 2020 core concepts project starter
+Starter project for part I of the React Redux with TypeScript 2020 workshop.  
+Generated with `create-react-app`.  
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container">
+            <h2>React Redux with TypeScript</h2>
+            <small>2020 Workshop</small>
+        </div>
+    );
+}
+
+export default App;
